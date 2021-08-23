06ee2642c1770369fd6918b3e57d98aa9a752ff7
Young Soo Kim <41531594+devYSK@users.noreply.github.com>
2021-08-23 13:56:20 +0900

    리액트 프로젝트 copy

---
+++ docker-react-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Reactadfadfadfasdfsadsfadsfsdfsdfsdfsdfsdfss
+          나는 항상 배가 고프다sdfsfdsd
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
